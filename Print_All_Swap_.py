a7f37e4805c7e661509077ce4cf40e6549988897
Kapellan <58063933+haider-bot@users.noreply.github.com>
2025-04-15 14:55:43 +0300

    Update Print_All_Swap_.py

--- Print_All_Swap_.py
+++ Print_All_Swap_.py
@@ -1,44 +1,29 @@
-def median_decorator(func):
-    def wrapper(self):
-        return func(self)
-    return wrapper
+from itertools import permutations
 
+def with_median(cls):
+    def mediana(self):
+        sorted_nums = sorted(self.nums)
+        n = len(sorted_nums)
+        if n % 2 == 1:
+            return sorted_nums[n // 2]
+        else:
+            return (sorted_nums[n // 2 - 1] + sorted_nums[n // 2]) / 2
+    cls.mediana = mediana
+    return cls
+
+@with_median
 class Class:
     def __init__(self, nums):
         self.nums = nums
         self.solution = []
 
     def permute(self):
-        self.solution = []
-        self._permute(self.nums, 0)
-
-    def _permute(self, arr, start):
-        if start == len(arr):
-            self.solution.append(arr[:])
-        else:
-            for i in range(start, len(arr)):
-                arr[start], arr[i] = arr[i], arr[start]
-                self._permute(arr, start + 1)
-                arr[start], arr[i] = arr[i], arr[start]
-
-    @median_decorator
-    def mediana(self):
-        a = sorted(self.nums)
-        n = len(a)
-        mid = n // 2
-        return a[mid] if n % 2 else (a[mid - 1] + a[mid]) / 2
+        self.solution = [list(p) for p in permutations(self.nums)]
 
     def __str__(self):
         return f"Для массива {self.nums} будут такие перестановки: {self.solution}"
 
 a = Class([1, 2, 3])
 a.permute()
-print(a)
-print(a.mediana())
-
-b = Class([0, 3])
-b.permute()
-print(b)
-print(b.mediana())
-
-
+print(a)            
+print(a.mediana())  
